3a43ec0a42a00ab195f63a42514168d9978f7b50
Baptiste de Minguine <baptiste.de-minguine@rubrash.com>
2022-12-07 09:01:14 +0100

    remove path in the createCookies

--- cookies.js
+++ cookies.js
@@ -4,7 +4,7 @@ function createCookie(name, value, days) {
     date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
     var expires = "; expires=" + date.toGMTString();
   } else var expires = "";
-  document.cookie = name + "=" + value + expires + "; path=/";
+  document.cookie = name + "=" + value + expires + ";";
 }
 function readCookie(name) {
   var nameEQ = name + "=";
